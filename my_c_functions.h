1f74a05b8304f35b7a72ed3938c85a21f8e550c9
mammadu diallo <diallo_m@qwasar.io>
2021-02-18 18:38:38 +0000

    UPDATE: added source and header files for linked lists and common C functions

---
+++ my_c_functions.h
@@ -0,0 +1,27 @@
+#ifndef my_c_functions_H
+#define my_c_functions_H
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>//
+
+char* my_strdup(char* param_1);
+
+char* combine_strings(char* first_string, char* second_string);
+
+int my_strlen(char* string);
+
+int my_recursive_pow(int param_1, int param_2);
+
+int my_atoi(char* param_1);
+
+void my_putstr(char* param_1);
+
+char* reverse_string(char* param_1);
+
+char hex_to_char (int num, char base);
+
+char *num_to_str(int num, char base);
+
+int my_strcmp(char* param_1, char* param_2);
+
+#endif
