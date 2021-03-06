b7568c4c0894e6af79f311fc07cb112b9ef91b90
jairo guzman <guzman_j@qwasar.io>
2021-03-06 18:29:29 +0000

    defined all linked list available functions before implementing linked list

--- linked_list.c
+++ linked_list.c
@@ -1,5 +1,7 @@
 #include "linked_list.h"
 
+//Creates new node "link" initializing it with a string
+//For my_tar puposes it will be the file name
 node* create_link_with_string(char* string)
 {
     node* link = malloc(sizeof(node));
@@ -8,6 +10,7 @@ node* create_link_with_string(char* string)
     return link;
 }
 
+//starts a link with memory allocated to receive a string later on
 node* create_link_with_max_string_length(int max_string_length)
 {
     node* link = malloc(sizeof(node));
@@ -16,6 +19,7 @@ node* create_link_with_max_string_length(int max_string_length)
     return link;
 }
 
+//adds a node before another node
 node* prepend_link(node* new_link, node* head)
 {
     new_link->next = head;
@@ -23,7 +27,7 @@ node* prepend_link(node* new_link, node* head)
     return head;
 }
 
-
+//loops to very end of Linked list and adds passed as argument
 void append_link(node* new_link, node* head)
 {   
     node* current = head;
@@ -34,12 +38,15 @@ void append_link(node* new_link, node* head)
     current->next = new_link;
 }
 
+//Insert node in between two nodes
 void insert_link(node* new_link, node* previous_link)
 {
     new_link->next = previous_link->next;
     previous_link->next = new_link;
 }
 
+//How many links are in a given linked list
+//Prints string to console
 int read_list(node* head)
 {  
     int index = 0;
@@ -52,6 +59,7 @@ int read_list(node* head)
     return index;
 }
 
+//return address of next link
 node* return_next_link(node* link)
 {
     node* next_link = link->next;
