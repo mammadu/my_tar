d50772c6d37edf6ae20df53b3e01327159057c45
jairo guzman <guzman_j@qwasar.io>
2021-03-27 23:06:04 +0000

    cleaning archive_parser.c

--- archive_parser.c
+++ archive_parser.c
@@ -73,47 +73,6 @@ int is_archive(char* file_path)
     }
 }
 
-// int check_existence(char* file_path)
-// {
-//     struct stat statbuf;
-//     int val = stat(file_path, &statbuf);
-//     return val;
-// }
-
-// int check_permission(char* file_path)
-// {
-//     struct stat statbuf;
-//     int status = stat(file_path, &statbuf);
-//     char* item_type_and_permissions = my_itoa_base(statbuf.st_mode, 8);
-//     int permission = item_type_and_permissions[my_strlen(item_type_and_permissions) - 3] - '0';
-//     free(item_type_and_permissions);
-//     return permission;
-// }
-
-// int initilize_archive_read(char* archive_name)
-// {
-//     int existence = check_existence(archive_name);
-//     if (existence == 0)
-//     {
-//         int permission = check_permission(archive_name);
-//         if (permission == 7 || permission == 6 || permission == 5 || permission == 4)
-//         {
-//             int fd = open(archive_name, O_RDONLY); //debug how to open files. Perhaps use check existance function????
-//             return fd;
-//         }
-//         else
-//         {
-//             printf("can't overwrite archive\n");
-//             return -1;
-//         }
-//     }
-//     else
-//     {
-//         printf("Archive does not exist\n");
-//         return -2;
-//     }
-// }
-
 //Creates a link. Reads from FD. 
 //Fills the string name, file contents, and points to null for next link
 node* fill_link(int fd)
@@ -238,27 +197,3 @@ node* extract_archive_to_node(char* archive_name, node* head, int fd)
 
     return head;
 }
-
-// int main(int argc, char** argv)
-// {
-//     int fd = initilize_archive_read(argv[2]);
-//     node* head = fill_link(fd);
-//     int current_position = lseek(fd, 0, SEEK_CUR);
-    
-//     //creates linked list of files from achive
-//     while (data_seeker(fd, current_position) > 0)
-//     {
-//         // current_position = lseek(fd, -1, SEEK_CUR);
-//         node* temp = fill_link(fd);
-//         append_link(temp, head);
-//         current_position = lseek(fd, 0, SEEK_CUR);
-//     }
-
-//     //creates files from linked list
-//     file_creator_from_list(head);
-
-//     free_linked_list(head);
-//     close(fd);
-    
-//     return 0;
-// }
